7180a2111f46ca2b80d5299f74f5d1053e417b40
gorani4545 <gorani4545@gmail.com>
2020-02-06 18:27:27 +0900

    2020-02-06

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                    Learn React
+                Learn React
                 </a>
               </header>
             </div>
