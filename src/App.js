1975a1186155df4353f997853437cdb00f5dd83e
gorani4545 <gorani4545@gmail.com>
2020-02-06 17:28:49 +0900

    2020-02-06 react, python 연동

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/api/');
+            const posts = await res.json();
+            this.setState({
+                posts
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {this.state.posts.map(item => (
+                    <p>
+                        {item.title}
+                    </p>
+                ))}
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+              </header>
+            </div>
+          );
+    }
 }
 
 export default App;
